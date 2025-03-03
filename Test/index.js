89660904dc2a8aca97155399fe1443424caf5927
digwijay-tech <digwijay2@gmail.com>
2025-03-04 00:12:11 +0530

    Started learning

---
+++ Test/index.js
@@ -0,0 +1,15 @@
+// document.getElementById("head").textContent='Welcome';
+// window.alert('Hello, World!');
+
+// let firstName= 'John';
+
+// console.log(typeof firstName);
+// console.log( `Your name is ${firstName}` );
+
+
+// let username;
+
+// document.getElementById("button").onclick=function(){
+//     username=document.getElementById("input").value;
+//     document.getElementById("head").textContent=`Welcome ${username}`;
+// }
